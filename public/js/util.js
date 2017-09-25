f3cf5367122def575420b9b45ebf1cabdbe783a2
mrname <123256@qq.com>
2017-09-25 20:01:52 +0800

    实现添加和编辑讲师页面共用[C

---
+++ public/js/util.js
@@ -0,0 +1,21 @@
+define(['jquery'], function ($) {
+    return {
+        qs: function (key) {
+            //获取URL参数中的指定的参数值
+            var param = location.search.substr(1);
+            // console.log(param);
+            var result = '';
+            if (param) {
+                var ps = param.split('&');  //以&截取，变成数组
+                $.each(ps, function (index, item) { //jquery遍历方法
+                    var kv = item.split('=');
+                    if (kv[0] == key) {
+                        result = kv[1];
+                        return false;   //终止each循环
+                    }
+                });
+            }
+            return result;
+        }
+    }
+});
