573ca2384cba6c728ee79bd91569822db2893326
mrname <123256@qq.com>
2017-09-27 00:58:24 +0800

    完成个人中心表单提交功能

--- public/js/settings.js
+++ public/js/settings.js
@@ -1,12 +1,12 @@
-define(['jquery','template','ckeditor','uploadify','region','datepicker','language'],function($,template,CKEDITOR){
+define(['jquery', 'template', 'ckeditor', 'uploadify', 'region', 'datepicker', 'language', , 'validate', 'form'], function ($, template, CKEDITOR) {
     //调用接口，获取个人信息
     $.ajax({
-        type:'get',
+        type: 'get',
         url: '/api/teacher/profile',
-        dataType : 'json',
-        success : function(data){
+        dataType: 'json',
+        success: function (data) {
             //解析数据，渲染页面
-            var html = template('settingsTpl',data.result);
+            var html = template('settingsTpl', data.result);
             $('#settingsInfo').html(html);
 
             //处理头像上传
@@ -14,14 +14,14 @@ define(['jquery','template','ckeditor','uploadify','region','datepicker','langua
                 width: 120,
                 height: 120,
                 buttonText: '',
-                itemTemplate:'<span><span>',
-                swf:'/public/assets/uploadify/uploadify.swf',
-                uploader:'/api/uploader/avatar',
-                fileObjName :  'tc_avatar',
-                onUploadSuccess : function(a,b){
+                itemTemplate: '<span><span>',
+                swf: '/public/assets/uploadify/uploadify.swf',
+                uploader: '/api/uploader/avatar',
+                fileObjName: 'tc_avatar',
+                onUploadSuccess: function (a, b) {
                     // console.log(b);
                     var obj = JSON.parse(b);
-                    $('.preview img').attr('src',obj.result.path);
+                    $('.preview img').attr('src', obj.result.path);
                 }
             });
 
@@ -32,6 +32,34 @@ define(['jquery','template','ckeditor','uploadify','region','datepicker','langua
 
             //处理富文本
             CKEDITOR.replace('editor');
+
+            //处理表单提交
+            $('#settingsForm').validate({
+                sendForm: false,
+                valid: function () {
+                    var p = $("#p").find('option:selected').text();
+                    var c = $("#c").find('option:selected').text();
+                    var d = $("#d").find('option:selected').text();
+                    var hometown = p + '|' + c + '|' + d;
+                    //同步富文本内容
+                    for(var instance in CKEDITOR.instances){
+                        CKEDITOR.instances[instance].updateElement();
+                    }
+                    //提交表单
+                    $(this).ajaxSubmit({
+                        type: 'post',
+                        url: '/api/teacher/modify',
+                        data:{tc_hometown:hometown},
+                        dataType: 'json',
+                        success: function (data) {
+                            if(data.code == 200){
+                                //修改成功之后刷新当前页面
+                                location.reload();
+                            }
+                        }
+                    });
+                }
+            });
         }
     });
 });
