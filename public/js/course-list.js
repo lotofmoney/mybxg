2cc35024467c3f45e9fee8824e9ca8cb3bb78d5d
mrname <123256@qq.com>
2017-09-27 11:10:26 +0800

    完成课程列表展示功能

--- public/js/course-list.js
+++ public/js/course-list.js
@@ -1,4 +1,15 @@
 define(['jquery','template','util'],function($,template,util){
     //设置左侧菜单选中功能
     util.setMenu(location.pathname);    
+    //获取所有的课程列表数据
+    $.ajax({
+        type: 'get',
+        url:'/api/course',
+        dataType : 'json',
+        success:function(data){
+            //解析数据，渲染页面
+            var html = template('courseTpl',{list:data.result});
+            $('#courseInfo').html(html);
+        }
+    });
 });
