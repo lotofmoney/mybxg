42f6d5bc7459faba93301a6fbfdea53c7e6c0447
mrname <123256@qq.com>
2017-09-28 01:04:59 +0800

    完成二级联动分类操作

--- public/js/course-basic.js
+++ public/js/course-basic.js
@@ -1,25 +1,44 @@
-define(['jquery','template','util'],function($,template,util){
-     //设置导航菜单选中
-     util.setMenu('/course/add');
-     //获取课程id
-     var csId = util.qs('cs_id');
-     //获取操作标志位
-     var flag = util.qs('flag');
-     //根据课程ID查询相关信息
-     $.ajax({
-        type :'get',
-        url : '/api/course/basic',
-        data:{cs_id:csId},
-        dataType:'json',
-        success:function(data){
+define(['jquery', 'template', 'util','ckeditor'], function ($, template, util,CKEDITOR) {
+    //设置导航菜单选中
+    util.setMenu('/course/add');
+    //获取课程id
+    var csId = util.qs('cs_id');
+    //获取操作标志位
+    var flag = util.qs('flag');
+    //根据课程ID查询相关信息
+    $.ajax({
+        type: 'get',
+        url: '/api/course/basic',
+        data: { cs_id: csId },
+        dataType: 'json',
+        success: function (data) {
             console.log(data);
-            if(flag){
+            if (flag) {
                 data.result.operate = '课程编辑';
-            }else{
+            } else {
                 data.result.operate = '课程添加';
             }
-            var html = template('basicTpl',data.result);
+            var html = template('basicTpl', data.result);
             $('#basicInfo').html(html);
+            //处理二级分类的下拉联动
+            $('#firstType').change(function () {
+                var pid = $(this).val();
+                //根据一级分类的id查询二级分类的数据
+                $.ajax({
+                    type: 'get',
+                    url: '/api/category/child',
+                    data: { cg_id: pid },
+                    dataType: 'json',
+                    success: function (data) {
+                        //拼接二级分类的下拉选项
+                        var tpl = '<option value="">请选择二级分类...</option>{{each list}}<option value="{{$value.cg_id}}">{{$value.cg_name}}</option>{{/each}}';
+                        var html = template.render(tpl, { list: data.result });
+                        $('#secondType').html(html);
+                    }
+                });
+            });
+            //处理富文本
+            CKEDITOR.replace('editor');
         }
-     });
+    });
 });
